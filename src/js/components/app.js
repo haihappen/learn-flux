2a7293a0c123cfbea35647144509ccc2163b442c
Mario Uher <uher.mario@gmail.com>
2014-10-14 09:40:19 +0200

    Scaffold

---
+++ src/js/components/app.js
@@ -0,0 +1,11 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+
+
+var app = React.createClass({
+  render: function() {
+    return <h1>my app</h1>
+  }
+})
+module.exports = app;
---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+/** @jsx React.DOM */
+
+var app = require('./components/app');
+var React = require('react');
+
+
+React.renderComponent(
+  <app />,
+  document.getElementById('main')
+);
