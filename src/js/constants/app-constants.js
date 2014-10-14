ad9d877f1778243e85f11dd4ff2fb7188a340ab7
Mario Uher <uher.mario@gmail.com>
2014-10-14 10:13:04 +0200

    Use keyMirror.

--- src/js/constants/app-constants.js
+++ src/js/constants/app-constants.js
@@ -1,6 +1,8 @@
-module.exports = {
-  ADD_ITEM: 'ADD_ITEM',
-  REMOVE_ITEM: 'REMOVE_ITEM',
-  INCREASE_ITEM: 'INCREASE_ITEM',
-  DECREASE_ITEM: 'DECREASE_ITEM'
-};
+var keyMirror = require('react/lib/keyMirror');
+
+module.exports = keyMirror({
+  ADD_ITEM: null,
+  REMOVE_ITEM: null,
+  INCREASE_ITEM: null,
+  DECREASE_ITEM: null
+});
